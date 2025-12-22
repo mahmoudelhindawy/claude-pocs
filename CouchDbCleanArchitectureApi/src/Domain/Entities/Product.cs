7579baeec088156084ca72016e40e51911ac1834
Mahmoud El Hindawy <mahmoud.elhendawy@gmail.com>
2025-12-22 10:43:48 +0300

    Add CouchDbCleanArchitectureApi/src/Domain/Entities/Product.cs

---
+++ CouchDbCleanArchitectureApi/src/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+}
