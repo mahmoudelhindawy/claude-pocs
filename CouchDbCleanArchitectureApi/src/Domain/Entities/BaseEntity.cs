1c81a48b3faae7571312d1a3388be5c311f0f0a7
Mahmoud El Hindawy <mahmoud.elhendawy@gmail.com>
2025-12-22 10:43:49 +0300

    Add CouchDbCleanArchitectureApi/src/Domain/Entities/BaseEntity.cs

---
+++ CouchDbCleanArchitectureApi/src/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+namespace Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string? Rev { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
