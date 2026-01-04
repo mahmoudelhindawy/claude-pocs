6292da6da493441c5a5f7f2793e59235bfdd99ef
Mahmoud El Hindawy <mahmoud.elhendawy@gmail.com>
2026-01-04 13:16:26 +0300

    Add CouchDbEFCore/src/Domain/Entities/Product.cs

---
+++ CouchDbEFCore/src/Domain/Entities/Product.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class Product : BaseEntity
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = string.Empty;
+    
+    [MaxLength(1000)]
+    public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    public decimal Price { get; set; }
+    
+    [Required]
+    public int Quantity { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Category { get; set; } = string.Empty;
+    
+    public bool IsActive { get; set; } = true;
+}
