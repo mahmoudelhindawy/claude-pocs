63da94735f5d00362fbba0d51dd0af9da66741ac
Mahmoud El Hindawy <mahmoud.elhendawy@gmail.com>
2026-01-07 18:40:04 +0300

    Update APIs to handle nested objects (Warehouse, Specifications)

--- CouchbaseEFCore/src/Application/Interfaces/IProductService.cs
+++ CouchbaseEFCore/src/Application/Interfaces/IProductService.cs
@@ -4,12 +4,26 @@ namespace Application.Interfaces;
 
 public interface IProductService
 {
-    Task<ProductDto?> GetProductByIdAsync(string id);
-    Task<IEnumerable<ProductDto>> GetAllProductsAsync();
-    Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(string category);
+    // Basic CRUD
+    Task<IEnumerable<ProductDto>> GetAllAsync();
+    Task<ProductDto?> GetByIdAsync(string id);
+    Task<ProductDto> CreateAsync(CreateProductDto createDto);
+    Task<ProductDto?> UpdateAsync(string id, UpdateProductDto updateDto);
+    Task<bool> DeleteAsync(string id);
+
+    // Query methods
+    Task<IEnumerable<ProductDto>> GetByCategoryAsync(string category);
     Task<IEnumerable<ProductDto>> GetActiveProductsAsync();
-    Task<IEnumerable<ProductDto>> SearchProductsByNameAsync(string searchTerm);
-    Task<ProductDto> CreateProductAsync(CreateProductDto productDto);
-    Task<ProductDto> UpdateProductAsync(string id, UpdateProductDto productDto);
-    Task<bool> DeleteProductAsync(string id);
+    Task<IEnumerable<ProductDto>> SearchByNameAsync(string searchTerm);
+
+    // Nested object queries
+    Task<IEnumerable<ProductDto>> GetByWarehouseCityAsync(string city);
+    Task<IEnumerable<ProductDto>> GetByWarehouseCodeAsync(string code);
+    Task<IEnumerable<ProductDto>> GetBySpecificationAsync(string key, string value);
+    Task<IEnumerable<ProductDto>> GetNearbyProductsAsync(double latitude, double longitude, double radiusKm);
+
+    // Nested object operations
+    Task<ProductDto?> UpdateWarehouseAsync(string id, WarehouseLocationDto warehouseDto);
+    Task<ProductDto?> AddSpecificationAsync(string id, ProductSpecificationDto specificationDto);
+    Task<ProductDto?> RemoveSpecificationAsync(string id, string key);
 }
